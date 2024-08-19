0aa04fed2b8f8f389292c0b91bee0b3084bfdb70
johnno1011 <jonathan@intellium.ai>
2024-08-19 14:14:53 +0000

    pr tweak

--- src/primaite/agents/aegis/prompts.py
+++ src/primaite/agents/aegis/prompts.py
@@ -20,14 +20,6 @@ For action 5, you must always specify the service ID to patch.
 
 You must always state which node number this action is to be applied to. If the action is a service patch, always specify which service id to patch.
 
-Here are some examples of actions in the format NODE_ID ACTION_ID
-Action: 'RESET 1'
-Action: 'PATCH_HARDWARE 2'
-Action: 'NONE'
-Action: 'PATCH SERVICE TCP 7'
-Action: 'TURN_OFF 3'
-Action: 'PATCH SERVICE UDP 5'
-
 Action: 1.1 - Turns on CLIENT_1
 Action: 2.3 - Resets CLIENT_2
 Action: 5.5.1 - Patches the TCP service for MANAGEMENT_CONSOLE
