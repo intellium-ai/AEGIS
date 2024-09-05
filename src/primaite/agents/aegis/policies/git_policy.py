4e95e5eb1c125786baf84566058f2336d93da5f6
Harvey <harvey@intellium.ai>
2024-09-05 14:17:32 +0000

    Removing Jacks stuff to make commits cleaner

--- src/primaite/agents/aegis/policies/git_policy.py
+++ src/primaite/agents/aegis/policies/git_policy.py
@@ -51,13 +51,6 @@ class GITPolicy(nn.Module):
             n_tokens=n_graph_tokens,
         ).to(self.ge_device)
 
-        # For tracking episode rewards and probs
-        self.roll_out = []
-        self.optimizer = Adam(self.parameters(), lr=learning_rate)
-
-    def put_data(self, data):
-        self.roll_out.append(data)
-
     def forward(self, graph_batch, action_prompt, reasoning_prompt):
         """This should return the action integer"""
 
@@ -91,45 +84,3 @@ class GITPolicy(nn.Module):
 
         logging.info(f"LLM Generated Reasoning: '{reasoning_statement}' with action '{response}'")
         return response, probs.squeeze(), reasoning_statement
-
-    def train_net(self, gamma: float = 0.99) -> Tuple[float, float]:
-        R = 0
-        G = []
-        G_t = 0
-
-        # Whitening baseline
-        for r, prob in self.roll_out[::-1]:
-            G_t = r + gamma * G_t
-            G.append(G_t)
-
-        G = np.array(G)
-        G_mean = G.mean()
-        G_std = G.std() + 1e-8  # add small episilon to avoid div by 0
-
-        # Check that the total reward is not 0
-        if sum(x[0] for x in self.roll_out) == 0:
-            self.roll_out = []  # reset
-            return 0, 0
-
-        # Calculate loss for the episode and do backprop
-        total_loss = 0
-        for r, prob in self.roll_out[::-1]:
-            R = r + gamma * R
-            for p in prob:
-                loss = -p * ((R - G_mean) / G_std)
-                total_loss += loss
-
-        total_loss.backward()
-        self.optimizer.step()
-
-        # Reset gradients
-        self.optimizer.zero_grad()
-
-        # Clear up any gpu memory that may be holding onto tensors unnecessarily
-        torch.cuda.empty_cache()
-
-        # Get average reward and reset rollout
-        mean_reward = np.mean([rew[0] for rew in self.roll_out])
-        self.roll_out = []
-
-        return total_loss.cpu().detach().numpy(), mean_reward
