724adc9e42cddf28cfe53df27b614842cd754e9e
johnno1011 <jonathan@intellium.ai>
2024-09-10 09:25:08 +0000

    prog

--- evaluation_scripts/evaluate_gnn_agent.py
+++ evaluation_scripts/evaluate_gnn_agent.py
@@ -2,16 +2,14 @@ from primaite.agents.gnn_agent import GNNAgent
 import os
 import pandas as pd
 
-lay_down_configs = os.listdir("../data/laydown_configs/")
-n_episodes = 5
+lay_down_configs = os.listdir("../data/datasets/GLLM_Dataset/")
 results = pd.DataFrame(columns=["lay_down_config", "avg_reward"])
 for lay_down in lay_down_configs:
-    print(lay_down)
-    lay_down_eval_rewards = []
-    agent = GNNAgent(
-            training_config_path="../agents/training_configs/gnn.yaml",
-            lay_down_config_path=f"../data/laydown_configs/{lay_down}",
-        )
-    for episode in range(n_episodes):
+    if lay_down.endswith('.yaml'):
+        lay_down_eval_rewards = []
+        agent = GNNAgent(
+                training_config_path="../agents/training_configs/gnn.yaml",
+                lay_down_config_path=f"../data/datasets/GLLM_Dataset/{lay_down}",
+            )
         avg_ep_reward = agent.evaluate()
         lay_down_eval_rewards.append(avg_ep_reward)
