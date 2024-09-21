723cf9ce92eb1606f69bbacd977e697cf44f5b76
johnno1011 <jonathan@intellium.ai>
2024-09-21 12:40:08 +0000

    gnn results are in

--- src/primaite/agents/gnn_agent.py
+++ src/primaite/agents/gnn_agent.py
@@ -5,6 +5,7 @@ from typing import Any
 import os
 import datetime
 import numpy as np
+import json
 
 import torch
 from torch.optim.lr_scheduler import CosineAnnealingLR
@@ -29,6 +30,9 @@ class Actor(nn.Module):
     def __init__(
         self, hidden_dim, n_gat_layers: int = 3, device: str = 'cuda:0'
     ):
+        self.hidden_dim = hidden_dim
+        self.n_gat_layers = n_gat_layers
+        self.device = device
         super().__init__()
         self.gat = GATConv(in_channels=6, out_channels=hidden_dim)
         self.inner_gat = GATConv(in_channels=hidden_dim, out_channels=hidden_dim)
@@ -73,10 +77,34 @@ class Actor(nn.Module):
         torch.nn.init.zeros_(self.linear.bias)
 
 
+    def save(self, path: str) -> None:
+        # Save init arguments
+        init_kwargs = {
+            'n_gat_layers' : self.n_gat_layers,
+            'hidden_dim' : self.hidden_dim,
+            'device' : self.device,
+        }
+
+        json.dump(init_kwargs, open(os.path.join(path, 'actor_init_kwargs.json'), 'w'))
+
+        # Save model
+        torch.save(self.state_dict(), os.path.join(path, 'actor.pt'))
+
+    @classmethod
+    def load(cls, path: str):
+        init_kwargs = json.load(open(os.path.join(path, 'actor_init_kwargs.json')))
+        ge = cls(**init_kwargs)
+        ge.load_state_dict(torch.load(os.path.join(path, 'actor.pt')))
+        return ge.to(ge.device)
+
 class Critic(nn.Module):
     def __init__(
         self, hidden_dim, n_gat_layers: int = 3, device: str = 'cuda:0'
-    ):
+    ):  
+        self.hidden_dim = hidden_dim
+        self.n_gat_layers = n_gat_layers
+        self.device = device
+        
         super().__init__()
         self.gat = GATConv(in_channels=6, out_channels=hidden_dim)
         self.inner_gat = GATConv(in_channels=hidden_dim, out_channels=hidden_dim)
@@ -114,6 +142,25 @@ class Critic(nn.Module):
         torch.nn.init.normal_(self.linear.weight, 1.0, 0.1)
         torch.nn.init.zeros_(self.linear.bias)
 
+    def save(self, path: str) -> None:
+        # Save init arguments
+        init_kwargs = {
+            'n_gat_layers' : self.n_gat_layers,
+            'hidden_dim' : self.hidden_dim,
+            'device' : self.device,
+        }
+
+        json.dump(init_kwargs, open(os.path.join(path, 'critic_init_kwargs.json'), 'w'))
+
+        # Save model
+        torch.save(self.state_dict(), os.path.join(path, 'critic.pt'))
+
+    @classmethod
+    def load(cls, path: str):
+        init_kwargs = json.load(open(os.path.join(path, 'critic_init_kwargs.json')))
+        ge = cls(**init_kwargs)
+        ge.load_state_dict(torch.load(os.path.join(path, 'critic.pt')))
+        return ge.to(ge.device)
 
 class GNNAgent(AgentSessionABC):
     def __init__(self, 
@@ -190,7 +237,7 @@ class GNNAgent(AgentSessionABC):
     def evaluate(
         self, 
         time_steps: int = 128,
-        episodes: int = 20,
+        episodes: int = 128,
         **kwargs
     ):
         self.is_eval = True
@@ -235,7 +282,6 @@ class GNNAgent(AgentSessionABC):
                 obs = new_obs
 
             reward_per_ep.append(np.sum(episode_rewards))
-        #self._env.close()
 
         return reward_per_ep
         
@@ -334,9 +380,6 @@ class GNNAgent(AgentSessionABC):
     
     def _get_latest_checkpoint(self) -> None:
         return #super()._get_latest_checkpoint()
-        
-    def evaluate(self, **kwargs: Any) -> None:
-        return #super().evaluate(**kwargs)
 
     @classmethod
     def load(cls, path):
--- training_scripts/train_gnn_agent.py
+++ training_scripts/train_gnn_agent.py
@@ -3,27 +3,55 @@ from primaite.agents.gnn_agent import GNNAgent
 import os
 import logging
 import random
+import pandas as pd
 logging.disable(logging.CRITICAL)
 training_laydowns = "/srv/aegis-training-data/laydowns/group_1/laydowns/"
+evaluation_laydowns = '/srv/aegis-evaluation-data/eval_dataset/'
 run_timestamp = datetime.now().strftime("%H-%M-%d-%m")
 save_path = f'/srv/aegis-trained-agents/gnn_{run_timestamp}/'
+corpus_sizes = ["5", "10", "20", "40"]
+eval_results_df = pd.DataFrame(columns=["corpus_size", "run", "episode", "sum_reward"])
+
+laydowns = os.listdir(training_laydowns)
+random.shuffle(laydowns)
+
+# train on 50 laydowns
+laydowns = laydowns[:50]
+
 if not os.path.exists(save_path):
     os.mkdir(save_path)
 first = True
-laydowns = os.listdir(training_laydowns)
-random.shuffle(laydowns)
 for idx, laydown in enumerate(laydowns):
+
     agent = GNNAgent(
         training_config_path="../agents/training_configs/gnn.yaml",
         lay_down_config_path=training_laydowns + laydown,
     )
     if not first:
-        agent._agent = agent._agent.load(save_path)
-    try:
-        agent.learn()
-    except Exception as e:
-        print(f'Something went wrong with laydown {laydown}:\n{e}')
-        pass
-    agent._agent.save(save_path)
+        agent.actor = agent.actor.load(save_path)
+        agent.critic = agent.critic.load(save_path)
+    agent.learn()
+    agent.actor.save(save_path)
+    agent.critic.save(save_path)
     first = False
-    print(f'Finished with laydown {idx} of {len(os.listdir(training_laydowns))}')
+    print(f'Finished with laydown {idx} of {len(laydowns)}')
+    
+# Now evaluate the model!
+for corpus_size in corpus_sizes:
+    agent = GNNAgent(
+        training_config_path="../agents/training_configs/gnn.yaml",
+        lay_down_config_path=evaluation_laydowns + corpus_size + '_nodes.yaml',
+    )
+    
+    agent.actor = agent.actor.load(save_path)
+    agent.critic = agent.critic.load(save_path)
+    
+    sum_ep_rewards = agent.evaluate()
+    for episode, reward in enumerate(sum_ep_rewards):
+        eval_results_df.loc[len(eval_results_df)] = {
+            "corpus_size": corpus_size,
+            "episode": episode + 1,
+            "sum_reward": reward,
+        }
+
+    eval_results_df.to_csv("gnn_eval_results.csv", index=False)
